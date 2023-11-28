db5a6cc4c1e81f616bf3557e3524cc577e8843f9
PKz <lightshadodo@gmail.com>
2023-11-28 07:46:52 +0700

    pic
    learning page pic

--- src/pages/learning.jsx
+++ src/pages/learning.jsx
@@ -2,12 +2,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
+
+const renderImg = (
+    <Box
+      component="img"
+      alt="text test"
+      src="/assets/Llist/fortinet-social-icon.jpg"
+      sx={{
+        top: 0,
+        width: 1,
+        height: 1,
+        objectFit: 'cover',
+        position: 'absolute',
+      }}
+    />
+  );
 
 export default function Learning() {
     return (
@@ -15,15 +32,19 @@ export default function Learning() {
             <Grid container spacing={3}>
                 {/* Row 1 */}
                 <Grid item xs={12} sm={12} md={4}>
-                    <Card fullWidth>
-                        <CardContent>
-                            <Typography variant="h5" component="div">
-                                Card 1
+                    <Card>
+                        <Box sx={{ pt: '100%', position: 'relative' }}>
+                            {renderImg}
+                        </Box>
+
+                        <Stack spacing={2} sx={{ p: 3 }}>
+                            <Typography color="inherit" underline="hover" variant="h5" noWrap>
+                                FortiGate
                             </Typography>
                             <Button component={Link} to="/lecture1" variant="contained" color="primary">
-                                Start
+                                Learn
                             </Button>
-                        </CardContent>
+                        </Stack>
                     </Card>
                 </Grid>
 
@@ -34,8 +55,8 @@ export default function Learning() {
                             <Typography variant="h5" component="div">
                                 Card 2
                             </Typography>
-                            <Button component={Link} to="/path2" variant="contained" color="primary">
-                                Start
+                            <Button component={Link} to="/lecture2" variant="contained" color="primary">
+                                Learn
                             </Button>
                         </CardContent>
                     </Card>
@@ -48,14 +69,14 @@ export default function Learning() {
                             <Typography variant="h5" component="div">
                                 Card 3
                             </Typography>
-                            <Button component={Link} to="/path3" variant="contained" color="primary">
-                                Start
+                            <Button component={Link} to="/lecture3" variant="contained" color="primary">
+                                Learn
                             </Button>
                         </CardContent>
                     </Card>
                 </Grid>
             </Grid>
-            <Grid container spacing={3} sx={{paddingTop:5}}>
+            <Grid container spacing={3} sx={{ paddingTop: 5 }}>
                 {/* Row 1 */}
                 <Grid item xs={12} sm={12} md={4}>
                     <Card fullWidth>
@@ -64,7 +85,7 @@ export default function Learning() {
                                 Card 1
                             </Typography>
                             <Button component={Link} to="/lecture1" variant="contained" color="primary">
-                                Start
+                                Learn
                             </Button>
                         </CardContent>
                     </Card>
@@ -78,7 +99,7 @@ export default function Learning() {
                                 Card 2
                             </Typography>
                             <Button component={Link} to="/path2" variant="contained" color="primary">
-                                Start
+                                Learn
                             </Button>
                         </CardContent>
                     </Card>
@@ -92,7 +113,7 @@ export default function Learning() {
                                 Card 3
                             </Typography>
                             <Button component={Link} to="/path3" variant="contained" color="primary">
-                                Start
+                                Learn
                             </Button>
                         </CardContent>
                     </Card>
